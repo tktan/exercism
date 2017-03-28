0ee988241d8756b4913481b24312703e89fb9056
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-03-28 23:17:41 +0800

    Add ruby/difference-of-squares

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,21 @@
+module BookKeeping
+  VERSION = 3
+end
+
+class Squares
+  def initialize(n)
+    @ns = *(1..n)
+  end
+
+  def square_of_sum
+    @ns.inject(0, :+) ** 2
+  end
+
+  def sum_of_squares
+    @ns.map {|n| n ** 2}.inject(0, :+)
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
