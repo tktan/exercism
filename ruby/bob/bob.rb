aa99233fb42f465b2d0d2d4f07731366c643fb89
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-07-18 15:25:13 +0800

    Update ruby/bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,8 +1,13 @@
 class Bob
-  def hey remark
-    return "Fine. Be that way!" if remark =~ /^\s*$/m && remark !~ /\S/
-    return "Whoa, chill out!" if remark == remark.upcase && remark != remark.downcase
-    return "Sure." if remark.strip[-1] == "?"
-    "Whatever."
+  def hey(remark)
+    if remark =~ /^\s*$/m && remark !~ /\S/
+      "Fine. Be that way!"
+    elsif remark == remark.upcase && remark != remark.downcase
+      "Whoa, chill out!"
+    elsif remark.strip[-1] == "?"
+      "Sure."
+    else
+      "Whatever."
+    end
   end
 end
