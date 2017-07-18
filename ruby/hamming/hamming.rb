a8e05b978bbcbf3a8d121fb831c41e65b49f0ea6
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-07-18 15:32:12 +0800

    Update ruby/hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -5,6 +5,6 @@ end
 class Hamming
   def self.compute(s1, s2)
     raise ArgumentError if s1.length != s2.length
-    s1.chars.zip(s2.chars).count {|x, y| x != y}
+    s1.chars.zip(s2.chars).count { |x, y| x != y }
   end
 end
